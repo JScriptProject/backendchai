999689fb83c327937aba9705435e4bc686c15996
Ravindra Kamble <write2ravi14@gmail.com>
2025-08-24 20:18:00 +0530

    ff

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const port = process.env.PORT || 5000;
 app.get('/', (req, res)=>{
     res.send("hello world");
 })
+app.get('/main', (req, res)=>{
+    res.send("<h1>Main page of the backend</h1>");
+})
 
 app.listen(port, ()=>{
     console.log(`Server at started on port number ${port}`);
